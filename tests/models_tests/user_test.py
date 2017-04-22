a492e3b1a14e452a3238755603f5d324a3281760
Roman Kovalenko <Roma@Romans-MacBook-Pro.local>
2017-04-22 20:23:55 +0300

    more asserts in User.get_recent() test

--- tests/models_tests/user_test.py
+++ tests/models_tests/user_test.py
@@ -12,3 +12,15 @@ class UserTest(OhmTestCase):
         assert len(rows) == 1
         rows = User.get_recent(3)
         assert len(rows) == 3
+        rows = User.get_recent(10)
+        assert len(rows) == 3
+        for row in rows:
+            if row['user_id'] == 1:
+                assert row['phones'] == ['+14086441234', '+14086445678']
+                assert row['location'] == 'EUROPE'
+            elif row['user_id'] == 2:
+                assert row['phones'] == ['+14086551234']
+                assert row['location'] is None
+            elif row['user_id'] == 3:
+                assert row['phones'] == []
+                assert row['location'] == 'USA'
